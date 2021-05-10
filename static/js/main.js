36e9e1babf4e1ccd104cb9feaf7d9e85435cd694
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-05-10 15:47:15 +0100

    Added searchbar & mobile flyout menu functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,33 @@
 const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuFirstDiv = document.querySelector('#mobile-menu > div');
 const mobileMenuButton = document.getElementById('mobile-menu-button');
-const mobileMenuIcon = document.getElementById('icon-menu-open');
-const mobileMenuIconClose = document.getElementById('icon-menu-close');
+const mobileMenuIcon = document.getElementById('mobile-menu-icon');
 
-// display mobile menu
-const changeMenuButton = () => {
+// displays mobile menu on click
+const displayMobileMenu = () => {
     mobileMenuButton.addEventListener('click', () => {
-        mobileMenuIcon.classList.toggle('hidden');
-        mobileMenuIconClose.classList.toggle('hidden');
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        mobileMenuFirstDiv.classList.toggle('hidden');
         mobileMenu.classList.toggle('mobile-menu-open');
+        mobileMenuIcon.classList.toggle('active');
     });
 };
-changeMenuButton();
+displayMobileMenu();
 
+const searchButton = document.getElementById('search-button');
+const searchBox = document.getElementById('search-box');
+const searchIcon = document.getElementById('search-icon');
+const searchIconClose = document.getElementById('search-icon-close');
+const searchField = document.getElementById('search-form');
+
+// displays search box on click & adds focus on input
+const displaySearchBox = () => {
+    searchButton.addEventListener('click', () => {
+        searchBox.classList.toggle('hidden');
+        searchIcon.classList.toggle('hidden');
+        searchIconClose.classList.toggle('hidden');
+
+        searchField.focus();
+    });
+};
+displaySearchBox();
