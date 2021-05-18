716799bc1c98699e0b7b5238dc2f969c47a05bc3
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-05-18 17:14:18 +0100

    Added basic functionality for checkout with stripe

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -5,9 +5,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = document.getElementById('id_stripe_public_key').innerHTML.slice(1, -1);
-var client_secret = document.getElementById('id_client_secret').innerHTML.slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = document.getElementById('id_stripe_public_key').innerHTML.slice(1, -1);
+var clientSecret = document.getElementById('id_client_secret').innerHTML.slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -31,7 +31,8 @@ card.mount('#card-element');
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
-        errorDiv.textContent = 'There seems to be a probelm processing your card right now'
+        errorDiv.textContent = 
+        "There seems to be a probelm processing your card right now please check you have the correct details"
     } else {
         errorDiv.textContent = '';
     }
@@ -39,31 +40,28 @@ card.addEventListener('change', function (event) {
 
 
 // Handle form submit
-// var form = document.getElementById('payment-form');
+var form = document.getElementById('payment-form');
+var submitButton = document.getElementById('submit-button');
 
-// form.addEventListener('submit', function(ev) {
-//     ev.preventDefault();
-//     card.update({ 'disabled': true});
-//     $('#submit-button').attr('disabled', true);
-//     stripe.confirmCardPayment(clientSecret, {
-//         payment_method: {
-//             card: card,
-//         }
-//     }).then(function(result) {
-//         if (result.error) {
-//             var errorDiv = document.getElementById('card-errors');
-//             var html = `
-//                 <span class="icon" role="alert">
-//                 <i class="fas fa-times"></i>
-//                 </span>
-//                 <span>${result.error.message}</span>`;
-//             $(errorDiv).html(html);
-//             card.update({ 'disabled': false});
-//             $('#submit-button').attr('disabled', false);
-//         } else {
-//             if (result.paymentIntent.status === 'succeeded') {
-//                 form.submit();
-//             }
-//         }
-//     });
-// });
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    card.update({ 'disabled': true});
+    submitButton.disabled = true;
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            errorDiv.textContent = 
+            "There seems to be a probelm processing your card right now please check you have the correct details"
+            card.update({ 'disabled': false});
+            submitButton.disabled = false;
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
