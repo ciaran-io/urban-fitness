2f3579d08354ea218788281b711ea3c5fa2ff70e
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-05-24 11:51:06 +0100

    Added post method to store billing details and meta data.

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -5,8 +5,12 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = document.getElementById('id_stripe_public_key').innerHTML.slice(1, -1);
-var clientSecret = document.getElementById('id_client_secret').innerHTML.slice(1, -1);
+var stripePublicKey = document
+    .getElementById('id_stripe_public_key')
+    .innerHTML.slice(1, -1);
+var clientSecret = document
+    .getElementById('id_client_secret')
+    .innerHTML.slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -16,52 +20,96 @@ var style = {
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
-            color: '#aab7c4'
-        }
+            color: '#aab7c4',
+        },
     },
     invalid: {
         color: '#dc3545',
-        iconColor: '#dc3545'
-    }
+        iconColor: '#dc3545',
+    },
 };
-var card = elements.create('card', {style: style});
+
+var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
-        errorDiv.textContent = 
-        "There seems to be a probelm processing your card right now please check you have the correct details"
+        errorDiv.textContent =
+            'There seems to be a probelm processing your card right now please check you have the correct details';
     } else {
         errorDiv.textContent = '';
     }
 });
 
-
 // Handle form submit
 var form = document.getElementById('payment-form');
+// form submit button
 var submitButton = document.getElementById('submit-button');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
-    card.update({ 'disabled': true});
+    card.update({ disabled: true });
     submitButton.disabled = true;
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            errorDiv.textContent = 
-            "There seems to be a probelm processing your card right now please check you have the correct details"
-            card.update({ 'disabled': false});
-            submitButton.disabled = false;
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
+
+    var saveInfo = Boolean(
+        document.querySelector('#id-save-info:checked').value
+    );
+    // From using {% csrf_token %} in the form
+    var csrfToken = document.querySelector(
+        'input[name="csrfmiddlewaretoken"]'
+    ).value;
+    var postData = {
+        csrfmiddlewaretoken: csrfToken,
+        client_secret: clientSecret,
+        save_info: saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    fetch(url, postData, {
+        method: 'POST',
     });
+
+    stripe
+        .confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: form.full_name.value.trim(),
+                    phone: form.phone_number.value.trim(),
+                    email: form.email.value.trim(),
+                    address: {
+                        line1: form.street_address1.value.trim(),
+                        line2: form.street_address2.value.trim(),
+                        city: form.town_or_city.value.trim(),
+                        state: form.county.value.trim(),
+                    },
+                },
+            },
+            shipping: {
+                name: form.full_name.value.trim(),
+                phone: form.phone_number.value.trim(),
+                address: {
+                    line1: form.street_address1.value.trim(),
+                    line2: form.street_address2.value.trim(),
+                    city: form.town_or_city.value.trim(),
+                    postal_code: form.postcode.value.trim(),
+                    state: form.county.value.trim(),
+                },
+            },
+        })
+        .then(function (result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                errorDiv.textContent =
+                    'There seems to be a probelm processing your card right now please check you have the correct details';
+                card.update({ disabled: false });
+                submitButton.disabled = false;
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
 });
