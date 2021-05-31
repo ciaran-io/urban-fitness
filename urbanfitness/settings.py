82e462de167328be2649868dc143da5544479da6
Ciaran <ciaran.io@icloud.com>
2021-05-31 03:59:42 +0100

    Removed secret key and set debug.

--- urbanfitness/settings.py
+++ urbanfitness/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%0czd!q6gn#v_w^&^%1a@xh2#e8escs#(pm2p1=8$n!rhle0y*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['urbanfitness.herokuapp.com', 'localhost']
 
